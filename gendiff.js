a5d727b199e1f4ed9a9bec5f24cb5d19c273f840
Hante-St <stepanenkova.katherine@gmail.com>
2025-09-21 10:30:21 +0300

    set up and configure environment

---
+++ gendiff.js
@@ -0,0 +1,13 @@
+#!/usr/bin/env node
+
+const { program } = require('commander');
+
+program
+  .name('gendiff')
+  .description('Compares two configuration files and shows a difference.')
+  .version('1.0.0');
+
+program.on('--help', () => {
+});
+
+program.parse(process.argv);
