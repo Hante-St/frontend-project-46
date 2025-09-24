fdb912068a51feeae2144a68273a42359fcb9e57
Hante-St <stepanenkova.katherine@gmail.com>
2025-09-24 21:35:41 +0300

    added files, parser.ja, src and __fixtures__; added arguments in gendiff.js

---
+++ parser.js
@@ -0,0 +1,15 @@
+import yaml from 'js-yaml';
+
+function parse(data, format) {
+  switch (format) {
+    case 'json':
+      return JSON.parse(data);
+    case 'yml':
+    case 'yaml':
+      return yaml.load(data);
+    default:
+      throw new Error(`Not correct format file ${data}`);
+  }
+}
+
+export default parse;
